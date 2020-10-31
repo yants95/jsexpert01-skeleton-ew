3cb25c3bce81175e61f920cecf6e8dfbcf7f5a1e
Yan Soares <yts.1995@gmail.com>
2020-10-30 21:02:35 -0300

    chore: fourth day done

---
+++ aula03/public/bandersnatch/src/network.js
@@ -0,0 +1,43 @@
+class Network {
+    constructor({ host }) {
+        this.host = host
+    }
+
+    parseManifestURL({ url, fileResolution, fileResolutionTag, hostTag }) {
+        return url.replace(fileResolutionTag, fileResolution).replace(hostTag, this.host)
+    }
+
+    async fetchFile(url) {
+        const response = await fetch(url)
+        return response.arrayBuffer()
+    }
+
+    async getProperResolution(url) {
+        const startMs = Date.now()
+        const response = await fetch(url)
+        await response.arrayBuffer()
+        const endMs = Date.now()
+        const durationInMs = (endMs - startMs)
+
+        const resolutions = [
+            //pior cenario possivel, 20 segundos
+            { start: 3001, end: 20000, resolution: 144 },
+
+            //ate 3 segundos
+            { start: 901, end: 3000, resolution: 360 },
+
+            //menos de 1 segundo
+            { start: 0, end: 900, resolution: 720 },
+        ]
+
+        const item = resolutions.find(item => {
+            return item.start <= durationInMs && item.end >= durationInMs
+        })
+
+        const LOWEST_RESOLUTION = 144
+
+        if (!item) return LOWEST_RESOLUTION
+
+        return item.resolution
+    }
+}
---
+++ aula03/public/bandersnatch/src/videoPlayer.js
@@ -0,0 +1,133 @@
+class VideoMediaPlayer {
+    constructor({ manifestJSON, network, videoComponent }) {
+        this.manifestJSON = manifestJSON
+        this.network = network
+        this.videoComponent = videoComponent
+
+        this.videoElement = null
+        this.sourceBuffer = null
+        this.activeItem = {}
+        this.selected = {}
+        this.videoDuration = 0
+        this.selections = []
+    }
+
+    initializeCodec() {
+        this.videoElement = document.getElementById('vid')
+        const mediaSourceSupporter = !!window.MediaSource
+
+        if (!mediaSourceSupporter) {
+            alert('Your browser or system does not support MSE!')
+        }
+
+        const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec)
+
+        if (!codecSupported) {
+            alert(`Your browser does not support the codec: ${this.manifestJSON.codec}`)
+            return;
+        }
+
+        const mediaSource = new MediaSource()
+        this.videoElement.src = URL.createObjectURL(mediaSource)
+
+        mediaSource.addEventListener('sourceopen', this.sourceOpenWrapper(mediaSource))
+    }
+
+    sourceOpenWrapper(mediaSource) {
+        return async(_) => {
+            this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec)
+            const selected = this.selected = this.manifestJSON.intro
+
+            //evita rodar como "LIVE"
+            mediaSource.duration = this.videoDuration
+            await this.fileDownload(selected.url)
+            setInterval(this.waitForQuestions.bind(this), 200)
+        }
+    }
+
+    waitForQuestions() {
+        const currentTime = parseInt(this.videoElement.currentTime)
+        const option = this.selected.at === currentTime
+
+        if (!option) return;
+        if (this.activeItem.url === this.selected.url) return;
+
+        this.videoComponent.configureModal(this.selected.options)
+        this.activeItem = this.selected
+    }
+
+    async currentFileResolution() {
+        const LOWEST_RESOLUTION = 144
+        const prepareUrl = {
+            url: this.manifestJSON.finalizar.url,
+            fileResolution: LOWEST_RESOLUTION,
+            fileResolutionTag: this.manifestJSON.fileResolutionTag,
+            hostTag: this.manifestJSON.hostTag
+        }
+
+        this.manageLag(this.selected)
+        const url = this.network.parseManifestURL(prepareUrl)
+        return this.network.getProperResolution(url)
+    }
+
+    async nextChunck(data) {
+        const key = data.toLowerCase()
+        const selected = this.manifestJSON[key]
+        this.selected = {
+            ...selected,
+            
+            //ajustar o tempo que o modal vai aparecer baseado no tempo corrente
+            at: parseInt(this.videoElement.currentTime + selected.at)
+        }
+
+        this.videoElement.play()
+        await this.fileDownload(selected.url)
+    }
+
+    manageLag(selected) {
+        if (!!~this.selections.indexOf(selected.url)) {
+            selected.at += 5
+            return;
+        }
+
+        this.selections.push(selected.url)
+    }
+
+    async fileDownload(url) {
+        const fileResolution = await this.currentFileResolution()
+        const prepareUrl = {
+            url,
+            fileResolution: fileResolution,
+            fileResolutionTag: this.manifestJSON.fileResolutionTag,
+            hostTag: this.manifestJSON.hostTag
+        }
+
+        const finalUrl = this.network.parseManifestURL(prepareUrl)
+        this.setVideoPlayerDuration(finalUrl)
+        const data = await this.network.fetchFile(finalUrl)
+        return this.processBufferSegments(data)
+    }
+
+    setVideoPlayerDuration(finalURL) {
+        const bars = finalURL.split('/')
+        const [, videoDuration] = bars[bars.length - 1].split('-')
+        this.videoDuration += parseFloat(videoDuration)
+    }
+
+    async processBufferSegments(allSegments) {
+        const sourceBuffer = this.sourceBuffer
+        sourceBuffer.appendBuffer(allSegments)
+
+        return new Promise((resolve, reject) => {
+            const updatedEnd = (_) => {
+                sourceBuffer.removeEventListener('updateend', updatedEnd)
+                sourceBuffer.timestampOffset = this.videoDuration
+
+                return resolve()
+            }
+
+            sourceBuffer.addeventListener('updateend', updatedEnd)
+            sourceBuffer.addeventListener('error', reject)
+        })
+    }
+}
